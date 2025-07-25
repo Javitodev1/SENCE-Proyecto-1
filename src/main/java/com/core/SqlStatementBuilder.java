3224d0c527f3de4674d2ee2d1f1a47727f489e4d
Javitodev <javito.dev@gmail.com>
2025-07-25 00:41:07 -0400

    statement build for book dao added

---
+++ src/main/java/com/core/SqlStatementBuilder.java
@@ -0,0 +1,24 @@
+package com.core;
+
+public class SqlStatementBuilder {
+
+    public static String createTable() {
+        return "CREATE TABLE IF NOT EXISTS books ( id INTEGER PRIMARY KEY, title TEXT NOT NULL, author TEXT NOT NULL, price REAL NOT NULL, discount REAL NOT NULL );";
+    }
+
+    public static String insertBook() {
+        return "INSERT INTO books (id, title, author, price, discount) VALUES (?, ?, ?, ?, ?)";
+    }
+
+    public static String updateBook() {
+        return "UPDATE books SET title = ?, author = ?, price = ?, discount = ? WHERE id = ?";
+    }
+
+    public static String deleteBook() {
+        return "DELETE FROM books WHERE id = ?";
+    }
+
+    public static String selectAllBooks() {
+        return "SELECT * FROM books";
+    }
+}
